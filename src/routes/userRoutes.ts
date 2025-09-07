70a9c6394d81e6c73bad58710442979ebb31f7ae
jj-int-dev <jjollpsl@gmail.com>
2025-09-07 05:42:36 -0400

    fix dotenv impoort. comment out auth middleware for testing purposes. pass in schema to drizzle initialization. create some files

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,8 +3,11 @@ import isAuthorized from '../utils/isAuthorized';
 
 const router = Router();
 
-router.get('/', isAuthorized, (req: Request, res: Response) => {
-  return res.status(200);
-});
+router.get(
+  '/',
+  /*isAuthorized,*/ (req: Request, res: Response) => {
+    return res.status(200).json({ body: 'good' });
+  }
+);
 
 export default router;
--- src/utils/databaseClient.ts
+++ src/utils/databaseClient.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import config from '../config/config';
+import * as schema from '../database-migrations/schema';
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(config.DATABASE_URL, { prepare: false });
-export const db = drizzle(client);
+export const db = drizzle({ client, schema });
